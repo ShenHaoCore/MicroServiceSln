54594468503d3c9b342305e5397f955ba2ddfe7a
ShenHaoCore <ShenHaoCore@163.com>
2022-12-13 23:32:35 +0800

    添加配置接口

---
+++ Base/Micro.BaseService.Behand/Controllers/ConfigController.cs
@@ -0,0 +1,23 @@
+﻿using Micro.Common;
+using Micro.Framework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Micro.BaseService.Behand.Controllers
+{
+    /// <summary>
+    /// 配置 控制器
+    /// </summary>
+    public class ConfigController : MicroController
+    {
+        /// <summary>
+        /// 获取
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Get([FromQuery] string path)
+        {
+            return Content(AppSetting.GetValue(path));
+        }
+    }
+}
